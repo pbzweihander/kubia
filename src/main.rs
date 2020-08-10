03746d4ca561ecca7bcd8b8898b5d2c8aa01c81f
rusty <rusty@spoqa.com>
2020-08-10 17:18:23 +0900

    Add POST endpoint

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,19 @@
 use {anyhow::Result, std::env::var, warp::Filter};
 
-#[tokio::main(core_threads = 8)]
+#[tokio::main]
 async fn main() -> Result<()> {
     simple_logger::init_with_level(log::Level::Info)?;
 
-    let filter = warp::get()
-        .map(|| format!("You've hit {}", var("HOSTNAME").unwrap()))
+    let filter = warp::any()
+        .and(warp::get().map(|| format!("You've hit {}", var("HOSTNAME").unwrap())))
+        .or(warp::post()
+            .and(warp::body::bytes())
+            .map(|body: bytes::Bytes| {
+                let body = String::from_utf8_lossy(&body);
+                log::info!("Posted: {}", body);
+                format!("You've posted:\n{}", body)
+            }))
+        .unify()
         .with(warp::log::log(env!("CARGO_PKG_NAME")));
     warp::serve(filter).run(([0, 0, 0, 0], 8080)).await;
 
